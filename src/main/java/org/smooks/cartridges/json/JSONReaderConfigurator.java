373a55bab08b2f86706ab2039ef9019480e248db
cjmamo <823038+claudemamo@users.noreply.github.com>
2024-03-11 09:37:21 +0100

    chore: prepare for development
    fix: solve errors caused by changes in Smooks dependencies
    
    docs: remove inherited obsolete ISSUE_TEMPLATE directory

--- src/main/java/org/smooks/cartridges/json/JSONReaderConfigurator.java
+++ src/main/java/org/smooks/cartridges/json/JSONReaderConfigurator.java
@@ -6,35 +6,35 @@
  * %%
  * Licensed under the terms of the Apache License Version 2.0, or
  * the GNU Lesser General Public License version 3.0 or later.
- * 
+ *
  * SPDX-License-Identifier: Apache-2.0 OR LGPL-3.0-or-later
- * 
+ *
  * ======================================================================
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * ======================================================================
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 3 of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -131,13 +131,13 @@ public class JSONReaderConfigurator implements ReaderConfigurator {
 
         configurator.getParameters().setProperty("rootName", rootName);
         configurator.getParameters().setProperty("arrayElementName", arrayElementName);
-        if(keyWhitspaceReplacement != null) {
+        if (keyWhitspaceReplacement != null) {
             configurator.getParameters().setProperty("keyWhitspaceReplacement", keyWhitspaceReplacement);
         }
-        if(keyPrefixOnNumeric != null) {
+        if (keyPrefixOnNumeric != null) {
             configurator.getParameters().setProperty("keyPrefixOnNumeric", keyPrefixOnNumeric);
         }
-        if(illegalElementNameCharReplacement != null) {
+        if (illegalElementNameCharReplacement != null) {
             configurator.getParameters().setProperty("illegalElementNameCharReplacement", illegalElementNameCharReplacement);
         }
         configurator.getParameters().setProperty("nullValueReplacement", nullValueReplacement);
@@ -146,12 +146,12 @@ public class JSONReaderConfigurator implements ReaderConfigurator {
         List<ResourceConfig> configList = configurator.toConfig();
         ResourceConfig resourceConfig = configList.get(0);
 
-        if(keyMap != null) {
+        if (keyMap != null) {
             Parameter<Map<String, String>> keyMapParam = new DefaultParameter<>(JSONReader.CONFIG_PARAM_KEY_MAP, keyMap);
             resourceConfig.setParameter(keyMapParam);
         }
 
-        resourceConfig.setTargetProfile(targetProfile);
+        resourceConfig.setProfile(targetProfile);
 
         return configList;
     }
--- src/main/java/org/smooks/cartridges/json/KeyMapDigester.java
+++ src/main/java/org/smooks/cartridges/json/KeyMapDigester.java
@@ -6,35 +6,35 @@
  * %%
  * Licensed under the terms of the Apache License Version 2.0, or
  * the GNU Lesser General Public License version 3.0 or later.
- * 
+ *
  * SPDX-License-Identifier: Apache-2.0 OR LGPL-3.0-or-later
- * 
+ *
  * ======================================================================
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * ======================================================================
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 3 of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -51,42 +51,42 @@ import java.util.HashMap;
 
 public final class KeyMapDigester {
 
-	private static final String KEY_MAP_KEY_ELEMENT = "key";
+    private static final String KEY_MAP_KEY_ELEMENT = "key";
 
-	private static final String KEY_MAP_KEY_ELEMENT_FROM_ATTRIBUTE = "from";
+    private static final String KEY_MAP_KEY_ELEMENT_FROM_ATTRIBUTE = "from";
 
-	private static final String KEY_MAP_KEY_ELEMENT_TO_ATTRIBUTE = "to";
+    private static final String KEY_MAP_KEY_ELEMENT_TO_ATTRIBUTE = "to";
 
-	private KeyMapDigester() {
+    private KeyMapDigester() {
 
-	}
+    }
 
-	public static HashMap<String, String> digest(Element keyMapElement) {
-		HashMap<String, String> keyMap = new HashMap<String, String>();
+    public static HashMap<String, String> digest(Element keyMapElement) {
+        HashMap<String, String> keyMap = new HashMap<String, String>();
 
-		NodeList keys = keyMapElement.getElementsByTagNameNS("*", KEY_MAP_KEY_ELEMENT);
+        NodeList keys = keyMapElement.getElementsByTagNameNS("*", KEY_MAP_KEY_ELEMENT);
 
-		for (int i = 0; i < keys.getLength(); i++) {
-			Element keyElement = (Element) keys.item(i);
+        for (int i = 0; i < keys.getLength(); i++) {
+            Element keyElement = (Element) keys.item(i);
 
-			String from = DomUtils.getAttributeValue(keyElement, KEY_MAP_KEY_ELEMENT_FROM_ATTRIBUTE);
+            String from = DomUtils.getAttributeValue(keyElement, KEY_MAP_KEY_ELEMENT_FROM_ATTRIBUTE);
 
-			if (from == null || from.trim().length() == 0) {
-				throw new SmooksConfigException("The '" + KEY_MAP_KEY_ELEMENT_FROM_ATTRIBUTE + "' attribute isn't defined or is empty for the key element: " + keyElement);
-			}
-			from = from.trim();
+            if (from == null || from.trim().length() == 0) {
+                throw new SmooksConfigException("The '" + KEY_MAP_KEY_ELEMENT_FROM_ATTRIBUTE + "' attribute isn't defined or is empty for the key element: " + keyElement);
+            }
+            from = from.trim();
 
-			String value = DomUtils.getAttributeValue(keyElement, KEY_MAP_KEY_ELEMENT_TO_ATTRIBUTE);
-			if (value == null) {
-				value = DomUtils.getAllText(keyElement, true);
-				if (value.trim().length() == 0) {
-					value = null;
-				}
-			}
-			keyMap.put(from, value);
-		}
+            String value = DomUtils.getAttributeValue(keyElement, KEY_MAP_KEY_ELEMENT_TO_ATTRIBUTE);
+            if (value == null) {
+                value = DomUtils.getAllText(keyElement, true);
+                if (value.trim().length() == 0) {
+                    value = null;
+                }
+            }
+            keyMap.put(from, value);
+        }
 
-		return keyMap;
-	}
+        return keyMap;
+    }
 
 }
