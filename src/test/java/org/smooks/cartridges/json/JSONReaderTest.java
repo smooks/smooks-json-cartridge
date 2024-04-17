b139deb073dbc75daedf2cb837fd74897a313a5b
cjmamo <823038+claudemamo@users.noreply.github.com>
2024-04-17 09:34:43 +0200

    test: solve compilation errors cause by upstream package rename

--- src/test/java/org/smooks/cartridges/json/JSONReaderTest.java
+++ src/test/java/org/smooks/cartridges/json/JSONReaderTest.java
@@ -58,7 +58,7 @@ import java.io.IOException;
 import java.util.Properties;
 
 import static org.junit.Assert.assertTrue;
-import static org.smooks.tck.Assertions.compareCharStreams;
+import static org.smooks.testkit.Assertions.compareCharStreams;
 
 /**
  * @author <a href="mailto:maurice@zeijen.net">maurice@zeijen.net</a>
